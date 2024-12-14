1e43f3149d32fed7af6f3c260910d7501b04f5e0
Rudranil <rudranild04@gmail.com>
2024-12-14 11:38:34 +0530

    Updated menuData.ts

--- src/utils/menuData.ts
+++ src/utils/menuData.ts
@@ -12,13 +12,19 @@ export const BENGALI_DISHES = {
       name: "Beguni",
       price: "₹120",
       description: "Crispy eggplant fritters served with kasundi mustard sauce",
-      image: "https://images.unsplash.com/photo-1601050690597-df0568f70950?ixlib=rb-4.0.3"
+      image: "https://www.scratchingcanvas.com/wp-content/uploads/2018/02/Beguni.1-801x1200.jpg"
     },
     {
       name: "Fish Chop",
       price: "₹180",
       description: "Bengali-style fish croquettes with beetroot kasundi",
-      image: "https://images.unsplash.com/photo-1600565193348-f74bd3c7ccdf?ixlib=rb-4.0.3"
+      image: "https://www.licious.in/blog/wp-content/uploads/2023/01/shutterstock_2197404569-600x400.jpg"
+    },
+    {
+      name: "Postor Bora",
+      price: "₹40",
+      description: "A common Bengali munchie (fritter) prepared with a paste of Poppyseed.",
+      image: "https://www.scratchingcanvas.com/wp-content/uploads/2018/05/Posto-Bora.3.jpg"
     }
   ],
   mainCourse: [
@@ -26,19 +32,19 @@ export const BENGALI_DISHES = {
       name: "Kosha Mangsho",
       price: "₹450",
       description: "Slow-cooked mutton in rich Bengali spices",
-      image: "https://images.unsplash.com/photo-1589302168068-964664d93dc0?ixlib=rb-4.0.3"
+      image: "https://www.freshtohome.com/blog/wp-content/uploads/2024/06/Screenshot-2024-06-26-130551.png"
     },
     {
       name: "Chingri Malai Curry",
       price: "₹550",
       description: "Prawns cooked in coconut cream curry",
-      image: "https://images.unsplash.com/photo-1516714435131-44d6b64dc6a2?ixlib=rb-4.0.3"
+      image: "https://khaddoroshik.com/wp-content/uploads/2024/01/Chingri-Macher-Malaikari-Recipe.webp"
     },
     {
       name: "Bhapa Ilish",
       price: "₹650",
       description: "Steamed Hilsa with mustard paste",
-      image: "https://images.unsplash.com/photo-1626844131082-256783844137?ixlib=rb-4.0.3"
+      image: "https://static.toiimg.com/photo/102574642/102574642.jpg"
     }
   ],
   desserts: [
@@ -46,13 +52,19 @@ export const BENGALI_DISHES = {
       name: "Nolen Gurer Payesh",
       price: "₹220",
       description: "Rice pudding with date palm jaggery",
-      image: "https://images.unsplash.com/photo-1551024506-0bccd828d307?ixlib=rb-4.0.3"
+      image: "https://www.vegrecipesofindia.com/wp-content/uploads/2017/11/bengali-rice-payesh-recipe-11-500x500.jpg"
     },
     {
       name: "Mishti Doi",
       price: "₹180",
       description: "Sweet fermented yogurt dessert",
-      image: "https://images.unsplash.com/photo-1617377321414-145d5401aa45?ixlib=rb-4.0.3"
+      image: "https://i0.wp.com/savorytales.com/wp-content/uploads/2017/11/IMG_6473.jpg?resize=707%2C471&ssl=1"
+    },
+    {
+      name: "Rosogolla",
+      price: "₹30",
+      description: "A syrupy dessert made from balls of chhena, an Indian cottage cheese, cooked in sugar syrup",
+      image: "https://assets.cntraveller.in/photos/626a486c6511cf2003cee4ab/4:3/w_1440,h_1080,c_limit/best-rosogolla-kolkata-2.jpg"
     }
   ]
 };
